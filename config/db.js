e39dbf665e3fc400849cfd46dc816025f0ade3ff
Sayantan Kapat <ksayantan1823@gmail.com>
2021-06-20 23:54:21 +0530

    connect the Mongo database

---
+++ config/db.js
@@ -0,0 +1,18 @@
+const config = require('config');
+const mongoose = require('mongoose');
+const db = config.get('mongoURI'); 
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(db);
+        console.log('database connected');
+    } catch(err) {
+        console.log(err.message);
+        process.exit(1);
+    }
+}
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
+const connectDB = require('./config/db');
 
 app.get('/', (req, res) => res.send('API Running'));
-
+connectDB();
 const PORT = process.env.PORT || 1821;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
