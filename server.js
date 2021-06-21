8d18bf64ff5bdf829cd073de1b4fa218d85abdd8
Sayantan Kapat <ksayantan1823@gmail.com>
2021-06-21 20:40:15 +0530

    create routes

--- server.js
+++ server.js
@@ -4,5 +4,10 @@ const connectDB = require('./config/db');
 
 app.get('/', (req, res) => res.send('API Running'));
 connectDB();
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/auth', require('./routes/api/auth'));
 const PORT = process.env.PORT || 1821;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
