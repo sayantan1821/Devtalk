b06566883af76d396664a066b303ef2ed4e4655e
Sayantan Kapat <ksayantan1823@gmail.com>
2021-06-23 23:35:08 +0530

    add user authentication

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,8 @@ const {check, validationResult} = require('express-validator');
 const User = require('../../models/Users');
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken'); 
+const config = require('config');
 
 //Register user
 
@@ -29,7 +31,7 @@ router.post(
         try {
             let user = await User.findOne({email}); 
             if(user) {
-                res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+                return res.status(400).json({ errors: [{ msg: 'User already exists' }] });
             }
 
             const avatar = gravatar.url(email, {
@@ -48,8 +50,20 @@ router.post(
             user.password = await bcrypt.hash(password, salt);
             await user.save();
 
-            res.send('User registered');
-
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            }
+            jwt.sign(
+                payload, 
+                config.get('jwtSecret'),
+                { expiresIn: 360000},
+                (err, token) => {
+                    if(err) throw err;
+                    res.json({token});
+                }
+            );
         } catch(err) {
             console.error(err.message);
             res.status(500).send('Server error');
