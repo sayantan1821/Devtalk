d5f5ea67b9d9b2e25c4ea42ee837c4b22c542d80
Sayantan Kapat <ksayantan1823@gmail.com>
2021-06-25 22:02:56 +0530

    update user auth

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,35 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const Profile = require('../../models/profile');
+const Users = require('../../models/Users');
+const {check, validationResult} = require('express-validator');
 
-router.get('/', (req, res) => res.send('profile route'));
+router.get('/me', auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.user.id }).populate('user',
+        ['name', 'avatar']);
+
+        if(!profile) {
+            return res.status(400).json({ mssg: 'No profile founded' });
+        }
+
+        res.json(profile);
+    } catch(err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+router.post('/', [auth, [
+    check('status', 'status is required').not().isEmpty(),
+    check('skills', 'skills is required').not().isEmpty()
+]],
+async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+}
+);
 
 module.exports = router;
