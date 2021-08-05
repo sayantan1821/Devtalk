778b520fa9fcd2da26113f8e87d8ab8c3dcc3006
sayantan1821 <ksayantan1823@gmail.com>
2021-08-06 00:00:10 +0530

    create experience route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -139,6 +139,54 @@ router.delete('/', auth, async (req, res) => {
 });
 
 //Add profile experience
+router.put('/experience', [auth,
+        [
+        check('title', 'Title is required')
+            .not()
+            .isEmpty(),
+        check('company', 'company is required')
+            .not()
+            .isEmpty(),
+        check('from', 'from date is required')
+            .not()
+            .isEmpty(),
+        ]        
+    ],
+    async(req, res) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()) {
+            return res.status(400).json({errors: errors.array() });
+        }
+
+        const {
+            title,
+            company,
+            location,
+            from,
+            to,
+            current,
+            description
+        } = req.body;
+
+        const newExp = {
+            company,
+            location,
+            from,
+            to,
+            current,
+            description
+        }
+        try {
+            const profile = await Profile.findOne({ user: req.user.id });
+
+            profile.experience.unshift(newExp);
+            await profile.save();
+            res.json(profile)
+        } catch(err) {
+            console.error(err.message);
+        }
+    }
+)
 
 
 module.exports = router;
