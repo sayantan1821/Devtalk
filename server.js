9d0e4fbf0bb941a4ba07a7670a10cb0714af3fb4
Sayantan Kapat <61209570+sayantan1821@users.noreply.github.com>
2021-08-18 03:35:58 +0530

    test from code sandbox

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const connectDB = require('./config/db');
+const connectDB = require("./config/db");
 
 connectDB(); //database connect
-app.get('/', (req, res) => res.send('API Running'));
+app.get("/", (req, res) => res.send("API Running"));
 
-app.use(express.json({ extended: false})); //middleware
+app.use(express.json({ extended: false })); //middleware
 
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/posts', require('./routes/api/posts'));
-app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/auth', require('./routes/api/auth'));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/auth", require("./routes/api/auth"));
 const PORT = process.env.PORT || 1821;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 
 // To start server command npm run server and check localhost:1821 posrt
+//test from code sandbox
