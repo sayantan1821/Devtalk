b193b2f6e407d1e275bccc202cfef43614dedc90
sayantan1821 <ksayantan1823@gmail.com>
2021-08-30 01:32:54 +0530

    add browser router

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
 import "./App.css";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
 import Landing from "./components/layouts/Landing";
 import Navbar from "./components/layouts/Navbar";
+import login from "./components/auth/login";
+import register from "./components/auth/register";
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Landing />
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/login" component={login} />
+          <Route exact path="/register" component={register} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ client/src/components/auth/login.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const login = () => {
+  return <div>Log in Here</div>;
+};
+
+export default login;
