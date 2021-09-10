31c4d2f89546ed4d08d59a029a81da7355382ad2
sayantan1821 <ksayantan1823@gmail.com>
2021-09-10 23:49:15 +0530

    add root reducer

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,45 @@
-import "./App.css";
-import { Switch, Route, BrowserRouter } from "react-router-dom";
-import Landing from "./components/layouts/Landing";
-import Navbar from "./components/layouts/Navbar";
-import login from "./components/auth/login";
-import register from "./components/auth/register";
-function App() {
+import React, { Fragment, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Routes from './components/routing/Routes';
+import { LOGOUT } from './actions/types';
+
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
+
+import './App.css';
+
+const App = () => {
+  useEffect(() => {
+    // check for token in LS
+    if (localStorage.token) {
+      setAuthToken(localStorage.token);
+    }
+    store.dispatch(loadUser());
+
+    // log user out from all tabs if they log out in one tab
+    window.addEventListener('storage', () => {
+      if (!localStorage.token) store.dispatch({ type: LOGOUT });
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <Switch>
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/login" component={login} />
-          <Route exact path="/register" component={register} />
-        </Switch>
-      </BrowserRouter>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route component={Routes} />
+          </Switch>
+        </Fragment>
+      </Router>
+    </Provider>
   );
-}
+};
 
 export default App;
