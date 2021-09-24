82e0084215ef98c28ba5796c09941bca3c99deea
sayantan1821 <ksayantan1823@gmail.com>
2021-09-24 23:34:25 +0530

    update frontend

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use("/api/users", require("./routes/api/users"));
 app.use("/api/posts", require("./routes/api/posts"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/auth", require("./routes/api/auth"));
-const PORT = process.env.PORT || 1821;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 
 // To start server command npm run server and check localhost:1821 posrt
