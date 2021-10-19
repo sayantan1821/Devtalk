d0cf7054c18dff29b79a132d68d4f4bbb43a89ea
Sayantan Kapat <61209570+sayantan1821@users.noreply.github.com>
2021-10-19 23:39:33 +0530

    update redux state

---
+++ client/src/actions/notify.js
@@ -0,0 +1,15 @@
+const names = ['James', 'John', 'Paul', 'Ringo', 'George'];
+
+function App() {
+  return (
+    <div>
+      {names.filter(name => name.includes('J')).map(filteredName => (
+        <li>
+          {filteredName}
+        </li>
+      ))}
+    </div>
+  );
+}
+
+export default App;
