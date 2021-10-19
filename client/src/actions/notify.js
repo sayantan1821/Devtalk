5e26bdeb97c9f24a84e80137e30e751be2592265
Sayantan Kapat <61209570+sayantan1821@users.noreply.github.com>
2021-10-19 23:40:02 +0530

    update alert component

--- client/src/actions/notify.js
+++ client/src/actions/notify.js
@@ -13,3 +13,18 @@ function App() {
 }
 
 export default App;
+const names = ['James', 'John', 'Paul', 'Ringo', 'George'];
+
+function App() {
+  return (
+    <div>
+      {names.filter(name => name.includes('J')).map(filteredName => (
+        <li>
+          {filteredName}
+        </li>
+      ))}
+    </div>
+  );
+}
+
+export default App;
